6c08c3bc3d3e806c1fd4012c72e39aaa61578dad
adam chen <tmac19920626@gmail.com>
2024-02-14 01:06:36 +0000

    3343

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -17,13 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    ppath("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
